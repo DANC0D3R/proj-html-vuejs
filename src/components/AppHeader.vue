55bbbe2c9b4c4d253772fc53cdc17bb09cff84b2
Daniele Minieri <danieleminieri99@gmail.com>
2023-02-14 12:17:12 +0100

    Struttura Header finita

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,10 @@
 <script>
+import { store } from '../store'
+
 export default {
 		data(){
 			return{
+                store,
 				socialIcons: [
 				"fa-brands fa-facebook-f",
 				"fa-brands fa-twitter",
@@ -16,6 +19,7 @@ export default {
 
 <template>
     <header>
+        <!-- Parte Superiore -->
 		<div class="my-container" id="top-container">
 			<div class="my-row-between">
                 <!-- Parte Sinistra -->
@@ -26,6 +30,7 @@ export default {
 							<span>Superhit Top Movie </span>
 							<p class="color-green">** King Star **</p>
 						</div>
+                        <!-- Icone Social -->
 						<ul>
 							<li v-for="(item,index) in socialIcons" :key="index">
 								<a href="#">
@@ -44,7 +49,44 @@ export default {
 				</div>
 			</div>
 		</div>
-		<hr class="border-grey">
+		<!-- Parte Centrale -->
+        <section>
+            <div class="my-container py-3">
+                <div class="my-row-between mb-4">
+                    <!-- Logo -->
+                    <div id="logo">
+                        <img src="../assets/img/cropped-logo.png" alt="logo">
+                    </div>
+                    <!-- Barra di Ricerca  -->
+                    <div id="searchbar" class="mt-4">
+                        <input type="text" class="border-secondary p-3" placeholder="Search ...">
+                        <button class="py-3 px-4">Search</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Barra di Navigazione  -->
+		<nav class="my-container">
+			<div class="my-row-between align-items-center">
+                <!-- Menu -->
+				<ul>
+					<li v-for="(item,index) in store.menu" :key="index">
+						<a href="#">
+							<h4>
+								{{ item.name }}
+								<!-- Se l'item ha un dropdwon al suo interno si visualizza l'arrow sown  -->
+								<i v-if="item.dropdownList" class="fa-solid fa-caret-down"></i>
+							</h4>
+						</a>
+
+					</li>
+				</ul>
+				<div id="cart">
+					<i class="fa-solid fa-basket-shopping"></i>
+					<span class="green-dot">0</span>
+				</div>
+			</div>
+		</nav>
 	</header>
 </template>
 
@@ -88,7 +130,6 @@ header{
             }
             .col-right{
                 #login{
-                    line-height: 20px;
                     i{
                         font-size: .8rem;
                     }
@@ -96,6 +137,65 @@ header{
                         vertical-align: middle;
                     }
                 }
+            }
+        }
+    }
+    section{
+        border-top: $border-blue-grey;
+        border-bottom: $border-blue-grey;
+        .my-row-between{
+            input{
+                width: 255px;
+
+                caret-color: $my-green;
+                background-color: $my-black;
+
+                outline: none;
+            }
+            button{
+                border: none;
+
+                background-color: $my-green;
+            }
+        }
+    }
+    nav{
+        ul{			
+            h4{
+                margin-bottom: 0;
+
+                padding: 2rem 2.5rem 2rem 0;
+
+                font-size: 1.2rem;
+                i{
+                    position: relative;
+
+                    font-size: 1.2rem;
+                }
+            }		
+        }
+        #cart{
+            position: relative;
+
+            color: #FFFFFF;
+            .green-dot{
+                position: absolute;
+                top: -12px;
+                left: 7px;
+                
+                display: inline-block;
+                text-align: center;
+                line-height: 1.2rem;
+
+                width: 1.2rem;
+                height: 1.2rem;
+
+                font-size: .75rem;
+                
+                border-radius: 50%;
+
+                color: #FFFFFF;
+                background-color: $my-green;
             }
         }
     }
--- src/store.js
+++ src/store.js
@@ -1,5 +1,94 @@
-import {reactive} from 'vue'
+import { reactive } from 'vue';
 
 export const store = reactive ({
-    
+	mediaCategories: [
+		{
+			name: 'All'
+		},
+		{
+			name: 'Coming Soon'
+		},
+		{
+			name: 'Latest Movie'
+		},
+		{
+			name: 'Top Rating'
+		},
+		{
+			name: 'TV Series'
+		}
+	],
+	menu: [
+		{
+			name: 'Home',
+			type: 'Dropdwon',
+			dropdownList: [
+				'Home',
+				'Home',
+				'Home',
+				'Home',
+				'Home',
+				'Home'
+			]
+		},
+		{
+			name: 'Celebrity',
+			type: 'Dropdwon',
+			dropdownList: [
+				'Celebrity',
+				'Celebrity Single'
+			]
+		},
+		{
+			name: 'Movie',
+			type: 'Dropdwon',
+			dropdownList: [
+				'Movie Post',
+				'Top Rate Movie',
+				'Movie Category',
+				'Single Movie'
+			]
+		},
+		{
+			name: 'Page',
+			type: 'Dropdwon',
+			dropdownList: [
+				'Pricing Table',
+				'Call To Action',
+				'My Account',
+				'Cart',
+				'Checkout',
+				'Wishlist',
+			]
+		},
+		{
+			name: 'Shop',
+			type: 'Dropdwon',
+			dropdownList: [
+				{
+					label: 'Shop Sidebar',
+					shopMenu: [
+						'Grid View',
+						'Last View'
+					]
+				},
+				{
+					label: 'No Sidebar',
+					shopMenu: [
+						'Grid View',
+						'Last View'
+					]
+				}
+			]
+
+		},
+		{
+			name: 'Blog',
+			type: 'link'
+		},
+		{
+			name: 'Contact Us',
+			type: 'link'
+		}
+	]
 })
