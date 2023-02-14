3ad9218e3cb965478b70d9cf818bbfd45f80cf98
Daniele Minieri <danieleminieri99@gmail.com>
2023-02-14 17:10:40 +0100

    Fix sorgente immagini

--- src/store.js
+++ src/store.js
@@ -24,31 +24,30 @@ export const store = reactive ({
 		"fa-brands fa-linkedin-in",
 		"fa-brands fa-instagram"
 	],
-    // DA FIXARE IMMAGINI (SORGENTE)
     posts: [
 		{
 			title: 'Actor The Concept Movie High Concept Theory see',
-			image: "./assets/img/blog-570x570.jpg",
+			image: "/src/assets/img/blog-570x570.jpg",
 			date: 'April 14, 2020',
 		},
 		{
 			title: 'consequat Nam lectus nibh aucibus It has turpis',
-			image: "./assets/img/blog2-570x570.jpg",
+			image: "/src/assets/img/blog2-570x570.jpg",
 			date: 'April 14, 2020',
 		},
 		{
 			title: 'Nam lectus nibh consequat aucibus turpis',
-			image: "./assets/img/blog3-570x570.jpg",
+			image: "/src/assets/img/blog3-570x570.jpg",
 			date: 'April 14, 2020',
 		},
 		{
 			title: 'Bring Me The Head Of Alfredo Garcia Best Movie In English 2020',
-			image: "./assets/img/blog4-570x570.jpg",
+			image: "/src/assets/img/blog4-570x570.jpg",
 			date: 'April 14, 2020',
 		},
 		{
 			title: 'Pump Action Interaction English Action Movie (2020 New)',
-			image: "./assets/img/blog5-570x570.jpg",
+			image: "/src/assets/img/blog5-570x570.jpg",
 			date: 'April 14, 2020',
 		},
 	],
